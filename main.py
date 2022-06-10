902d1b326fc00de05761e2ff97980c060ec297cd
pcatto <tickcatto@gmail.com>
2022-06-10 11:30:00 -0300

    Alterando lista

--- main.py
+++ main.py
@@ -66,12 +66,74 @@ class inputAdd(Screen):
 
 # APP
 class TestApp(MDApp):
+
+
+
+    #Build the app
     def build(self):
-        return Builder.load_file('screens/screenManager.kv')
+        print("aaaaa")
+        ScreenManager = Builder.load_file('screens/screenManager.kv')
+
+
+
+        return ScreenManager
+
+
+
+    def show_table_menu(self) -> None:
+        '''Called when a table is clicked.'''
+
+        
+        layout = MDFloatLayout()  # root layout
+        # Creating control buttons.
+        button_box = MDBoxLayout(
+            pos_hint={"center_y": 0.3, "center_x": 0.5},
+            adaptive_size=True,
+            padding="24dp",
+            spacing="24dp",
+        )
+
+
+        button_box.add_widget(
+            MDRaisedButton(
+                text="Adicionar"#, on_release=self.
+            )
+        )
+        button_box.add_widget(
+            MDRaisedButton(
+                text="Remover"#, on_release=self.
+            )
+        )
+
+
+        # Create a table.
+        self.data_tables = MDDataTable(
+            pos_hint={"center_y": 0.7, "center_x": 0.5},
+            size_hint=(0.9, 0.7),
+            use_pagination=False,
+            column_data=[
+                ("Membro", dp(30)),
+                ("Participação", dp(20)),
+                ("Contribuição", dp(20)),
+            ],
+            row_data=[("1", "1", "2"), ("2", "2", "3"), ("3", "3", "4")],
+        )
+
+
+
+        # Adding a table and buttons to the toot layout.
+        layout.add_widget(self.data_tables)
+        layout.add_widget(button_box)
+
+        return layout
 
 
 
 
+
+
+
+###############################################################################
     #date picker
 
     def on_save(self, instance, value, date_range):
@@ -99,64 +161,14 @@ class TestApp(MDApp):
         date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
         date_dialog.open()
 
+
+
+###############################################################################
     #Table of Members
 
-    def show_tabela(self):
-        print("aaaaa")
-        layout = MDFloatLayout()  # root layout
-        # Creating control buttons.
-        button_box = MDBoxLayout(
-            pos_hint={"center_x": 0.5},
-            adaptive_size=True,
-            padding="24dp",
-            spacing="24dp",
-        )
 
-        for button_text in ["Add row", "Remove row"]:
-            button_box.add_widget(
-                MDRaisedButton(
-                    text=button_text, on_release=self.on_button_press
-                )
-            )
 
-        # Create a table.
-        self.data_tables = MDDataTable(
-            pos_hint={"center_y": 0.5, "center_x": 0.5},
-            size_hint=(0.9, 0.6),
-            use_pagination=False,
-            column_data=[
-                ("No.", dp(30)),
-                ("Column 1", dp(40)),
-                ("Column 2", dp(40)),
-                ("Column 3", dp(40)),
-            ],
-            row_data=[("1", "1", "2", "3")],
-        )
-        # Adding a table and buttons to the toot layout.
-        layout.add_widget(self.data_tables)
-        layout.add_widget(button_box)
-        
 
-        return layout
-
-    def on_button_press(self, instance_button: MDRaisedButton) -> None:
-        '''Called when a control button is clicked.'''
-
-        try:
-            {
-                "Add row": self.add_row,
-                "Remove row": self.remove_row,
-            }[instance_button.text]()
-        except KeyError:
-            pass
-
-    def add_row(self) -> None:
-        last_num_row = int(self.data_tables.row_data[-1][0])
-        self.data_tables.add_row((str(last_num_row + 1), "1", "2", "3"))
-
-    def remove_row(self) -> None:
-        if len(self.data_tables.row_data) > 1:
-            self.data_tables.remove_row(self.data_tables.row_data[-1])
 
 
 ###############################################################################
